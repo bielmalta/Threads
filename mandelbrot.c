e8517cecc828549561fe0e89ff149d66b4497e1e
Gabriel Malta <ggm@cesar.school>
2026-08-27 15:14:52 -0300

    implementando a função de calcular os pixels

---
+++ mandelbrot.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int calcular_pixel(int x, int y, int largura, int altura, int max_iter){
+    double cr = -2.0 + (3.0 * x) / largura;
+    double ci = -1.5 + (3.0 * y) / altura;
+    double zr = 0.0;
+    double zi = 0.0;
+    int iter = 0;
+
+    while (zr * zr + zi * zi <= 4.0 && iter < max_iter) {
+        double novo_zr = zr * zr - zi * zi + cr;
+        zi = 2.0 * zr * zi + ci;
+        zr = novo_zr;
+
+        iter++;
+    }
+    return (iter * 255) / max_iter;
+}
